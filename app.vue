7fd8980d5aa6d9267f165da31257b22f80606eda
Muhdani Boyrend Erlan Azhari <muhdaniboyrendi@gmail.com>
2025-04-17 16:55:33 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="relative font-primary min-h-screen overflow-hidden"
+  >
+    <Header />
+    <div class="absolute inset-0 bg-gradient-to-br from-blue-50 to-indigo-100"></div>
+    
+    <div class="absolute -top-100 -right-100 w-200 h-200 rounded-full bg-indigo-500/20 blur-xs"></div>
+    <div class="relative z-10 container mx-auto pt-30">
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  </div>
+</template>
